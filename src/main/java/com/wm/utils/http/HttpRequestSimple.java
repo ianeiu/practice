ffc9889de2b6603f98c517b9b8cc524cf162f8b6
amian <weimianwu@foxmail.com>
2018-07-13 15:56:32 +0800

    新增笔记；修改logback.xml
    Signed-off-by: amian <weimianwu@foxmail.com>

--- src/main/java/com/wm/utils/http/HttpRequestSimple.java
+++ src/main/java/com/wm/utils/http/HttpRequestSimple.java
@@ -13,13 +13,12 @@ public class HttpRequestSimple {
     /**
      * 向指定URL发送GET方法的请求
      * 
-     * @param url
-     *            发送请求的URL
-     * @param param
-     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @param url 发送请求的URL
+     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
      * @return URL 所代表远程资源的响应结果
+     * @throws IOException 
      */
-    public static String sendGet(String url, String param) {
+    public static String sendGet(String url, String param) throws IOException {
         String result = "";
         BufferedReader in = null;
         try {
@@ -30,8 +29,7 @@ public class HttpRequestSimple {
             // 设置通用的请求属性
             connection.setRequestProperty("accept", "*/*");
             connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             // 建立实际的连接
             connection.connect();
             // 获取所有响应头字段
@@ -47,10 +45,7 @@ public class HttpRequestSimple {
             while ((line = in.readLine()) != null) {
                 result += line;
             }
-        } catch (Exception e) {
-            System.out.println("发送GET请求出现异常！" + e);
-            e.printStackTrace();
-        }
+        } 
         // 使用finally块来关闭输入流
         finally {
             try {
@@ -67,13 +62,12 @@ public class HttpRequestSimple {
     /**
 	 * 向指定 URL 发送POST方法的请求
 	 * 
-	 * @param url
-	 *            发送请求的 URL
-	 * @param param
-	 *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+	 * @param url  发送请求的 URL
+	 * @param param   请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
 	 * @return 所代表远程资源的响应结果
+     * @throws IOException 
 	 */
-    public static String sendPost(String url, String param) {
+    public static String sendPost(String url, String param) throws IOException {
         PrintWriter out = null;
         BufferedReader in = null;
         String result = "";
@@ -84,8 +78,7 @@ public class HttpRequestSimple {
             // 设置通用的请求属性
             conn.setRequestProperty("accept", "*/*");
             conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
@@ -96,16 +89,12 @@ public class HttpRequestSimple {
             // flush输出流的缓冲
             out.flush();
             // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line;
             while ((line = in.readLine()) != null) {
                 result += line;
             }
-        } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
-            e.printStackTrace();
-        }
+        } 
         //使用finally块来关闭输出流、输入流
         finally{
             try{

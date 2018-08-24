6759d2fef8f2f109801ea78b0ad2c83c6aabe9c9
amian <weimianwu@foxmail.com>
2018-08-24 17:01:52 +0800

    新增DEMO
    Signed-off-by: amian <weimianwu@foxmail.com>

--- src/main/java/com/wm/demo/trywork/Demo03StringFun.java
+++ src/main/java/com/wm/demo/trywork/Demo03StringFun.java
@@ -1,5 +1,8 @@
 package com.wm.demo.trywork;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 public class Demo03StringFun {
@@ -26,6 +29,12 @@ public class Demo03StringFun {
 		System.out.println(str.contains("holl"));//false
 	}
 	
+	/**
+	 * @Description:  ==
+	 * @author: wm
+	 * @date: 2018年8月24日 下午2:09:13
+	 * @version: 1.0
+	 */
 	@Test
 	public void testDD(){
 		String str = "helo";
@@ -34,5 +43,13 @@ public class Demo03StringFun {
 		
 		System.out.println(str3); //helo
 		System.out.println(str == str3); //false
+	}
+	
+	@Test
+	public void testJoin(){
+		List<String> list = Arrays.asList("1","2","3");
+		List<String> list2 = Arrays.asList();
+		System.out.println(String.join(";", list));
+		System.out.println(String.join(";", list2));
 	}
 }
---
+++ src/main/java/com/wm/demo/trywork/Demo16.java
@@ -0,0 +1,9 @@
+package com.wm.demo.trywork;
+
+import java.util.List;
+
+public class Demo16 {
+	public static void main(String[] args) {
+		
+	}
+}

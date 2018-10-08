54d7c4d2fda82fb96ea644bb3dfaa3b8cbc4e5ff
amian <weimianwu@foxmail.com>
2018-10-08 16:07:17 +0800

    add code
    Signed-off-by: amian <weimianwu@foxmail.com>

--- src/main/java/com/wm/demo/patterns/build/factorySimple/MusicBoxFactory.java
+++ src/main/java/com/wm/demo/patterns/build/factorySimple/MusicBoxFactory.java
@@ -7,7 +7,7 @@ public class MusicBoxFactory {
 
 		// 这边使用的是Java的Reflection机制来产生实例
 		// 不过客户端不用管啦
-		// 以后就算改变了这边的程式，客户端程式是不用更改的
+		// 以后就算改变了这边的程式，客户端程是不用更改的
 		return (IMusicBox) Class.forName(name).newInstance();
 
 	}
